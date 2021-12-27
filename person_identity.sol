0b5a6a087c1b3f02233bed3674856a19671e8065
Amritanshu Raj <83136054+amritanshu19@users.noreply.github.com>
2021-12-27 14:45:31 +0530

    Create person_identity.sol

---
+++ person_identity.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.24;  //specifies the version of solidity used.
+
+contract person_identity
+{
+    
+    //declare a string variable for the name of the person
+    string name;
+    
+    //following function gives the name in the contract
+    function get_name() public view returns(string)
+    {
+        return name;
+    }
+    
+    //following function sets the name in the contract
+    function set_name(string _name) public
+    {
+        name = _name;
+    }
+    
+    //repeat the same above process for age
+    uint age;       //uint-> unsigned integer
+    
+    function get_age() public view returns(uint)
+    {
+        return age; 
+    }
+    
+    function sets_age(uint _age) public
+    {
+        age = _age; 
+    }
+
+}
