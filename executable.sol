82db4cfc12bf466a6b626193bf10d9cec8f23855
Amritanshu Raj <83136054+amritanshu19@users.noreply.github.com>
2023-02-17 23:33:04 +0530

    Create executable.sol

---
+++ executable.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity = 0.8.17;
+/**
+   * @title SimpleStorage
+   * @dev ContractDescription
+   * @custom:dev-run-script ./artifacts/internship.json
+   */
+contract internship{
+    address public intern; // stores my ethereum address
+    uint public time_stamp; //stores unix time stamp like: 1676644796
+
+    constructor (address _mine, uint timeTopay) payable{
+        time_stamp = block.timestamp + timeTopay;
+        intern = _mine;
+    }
+    function withdraw() external {
+        require(block.timestamp>=time_stamp, 'Not ready yet');
+        require(msg.sender == intern, 'Only i can withdraw');
+        payable(msg.sender).transfer(address(this).balance); 
+    }
+}
